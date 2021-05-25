3dc8cdd1a2993f0b06aa628a7bc4774c045d299d
Miro <MiroPhoenix91@gmail.com>
2021-05-25 23:14:55 +0200

    Dc Heros

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Dc Heros</h1>
+    <ul>
+      <li v-for="(hero, index) in Heros" :key="index">{{hero.name}}</li>
+    </ul>
+    <form @submit.prevent="addHero">
+      <input type="text" v-model="newHero">
+      <button type="submit" >Add Hero</button>
+    </form>
+</template>
+
+<script>
+import { reactive ,ref} from 'vue'
+
+export default {
+  name: 'App',
+  components: {},
+
+  setup () {
+    // const isDisabled = ref(false)
+    const newHero = ref('')
+
+    const Heros = reactive([
+        {name: 'SuperGirl'},
+        { name: 'Flash'},
+        { name: 'Arrow'},
+        { name: 'Batman'},
+        {name: 'Superman'}
+    ])
+
+    const addHero = () => {
+      if(newHero.value !== '') {
+        Heros.push({name: newHero.value})
+        newHero.value = ''
+      }
+    }
+
+
+    return { Heros , newHero, addHero}
+  }
+}
+</script>
+
+<style scoped>
+</style>
